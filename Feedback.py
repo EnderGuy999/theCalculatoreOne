1cfe89d60c974fe8e3b36ee6d4589175b8cae73f
Robert <robertg@crabtreemail.net>
2016-08-17 19:46:13 -0400

    Expanded On Ideas

--- Feedback.py
+++ Feedback.py
@@ -1,16 +1,28 @@
-import os
 
 
 def feedback():
     inputFeedback = input("Do you want to give feedback.(yes or no)\n")
     if "y" in inputFeedback:
+        name = input("Would you please put your username.\n")
         feedbackText = input("Type your thoughts now.\n")
-        newFolder = r'C:\Program Files\feedback'
-        if not os.path.exists(newFolder):
-            os.makedirs(newFolder)
+        #   nameFile = username + "_" + date
+
+        #   if nameFile exists:
+        #       ERROR file already exists
+        #       quit()
+        #   else:
+        #       create the file with the name being nameFile
+
+
         #   parce data with return every 15 words.
+
+
         #   store parced data in text document.
+
+
         #   close out of document and program.
+
+
         return
     elif "n" in inputFeedback:
         return
